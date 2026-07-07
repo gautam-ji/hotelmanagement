3241c2f967845bba8d475280a447ce469e377910
Gautam Ji <gautam123@gmail.com>
2026-07-07 14:23:22 +0530

    create a updateReview Function

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -1,62 +1,81 @@
 import reviewModel from "../models/reviewModel.js";
-import listModel from  '../models/listModel.js'
+import listModel from "../models/listModel.js";
 
-const createReview  = async (req,res) => {
-    try{
-       
-    const {userId,ListingId,comment,rating} = req.body;
+const createReview = async (req, res) => {
+  try {
+    const { userId, ListingId, comment, rating } = req.body;
 
-    if(!userId || !ListingId || !comment || !rating){
-     return res.json({success:false,message:"Required All fields"})
+    if (!userId || !ListingId || !comment || !rating) {
+      return res.json({ success: false, message: "Required All fields" });
     }
 
     const newReview = new reviewModel({
-      user:userId,
-      listing:ListingId, 
-      comment:comment,
-      rating:Number(rating)
-    })
+      user: userId,
+      listing: ListingId,
+      comment: comment,
+      rating: Number(rating),
+    });
 
-   await newReview.save();
+    await newReview.save();
 
-   //Recalculate average rating 
-   const reviews = await reviewModel.find({listing: ListingId});
+    //Recalculate average rating
+    const reviews = await reviewModel.find({ listing: ListingId });
 
-   const avgRating = reviews.reduce((acc,r) => acc + r.rating, 0) / reviews.length;
+    const avgRating =
+      reviews.reduce((acc, r) => acc + r.rating, 0) / reviews.length;
 
-   await listModel.findByIdAndUpdate(ListingId, {rating: avgRating});
+    await listModel.findByIdAndUpdate(ListingId, { rating: avgRating });
+
+    return res.json({
+      success: true,
+      message: "Review Created Successfully",
+      newReview,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+const updateReview = async (req, res) => {
+  try {
+    const { reviewId, rating, comment } = req.body;
 
 
-   
-   return res.json({success:true,message:"Review Created Successfully",newReview})
-    
-        
-
-    } catch(error){
-      console.error(error)
-    return  res.json({success:false,message:error.message})
+    if (!reviewId || !rating || !comment) {
+      return res.json({ success: false, message: "all fields are required" });
     }
-}
 
-const updateReview = async(req,res) =>{
-     try{
+    const reviewData = {
+      rating: Number(rating),
+      comment: comment,
+    };
 
-    } catch(error){
-      console.error(error)
-      res.json({success:false,message:error.message})
-    }
-} 
+    const newReview = await reviewModel.findByIdAndUpdate(
+      reviewId,
+      reviewData,
+      { returnDocument: 'after' }
+    );
 
-const deleteReview = async (req, res) =>{
-     try{
+    await newReview.save();
 
-    } catch(error){
-      console.error(error)
-      res.json({success:false,message:error.message})
-    }
-}
+    return res.json({
+      success: true,
+      message: "Review Update Successfully ",
+      newReview,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
+const deleteReview = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-
-
-export {createReview,updateReview,deleteReview}
+export { createReview, updateReview, deleteReview };
